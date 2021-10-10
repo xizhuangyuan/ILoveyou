1edc3163ebaa8314b20f435d5aa3efda0d8764a6
xixiaolu <87653023+xixiaolu@users.noreply.github.com>
2021-10-10 08:42:32 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 document.getElementsByClassName('disappear')[0].onclick = function(){
         document.getElementById('wrap').setAttribute('class','box')
-}
+    }
+    setTimeout(() => {
+        document.getElementsByClassName('disappear')[0].style.display = 'block';
+    }, 15000);
