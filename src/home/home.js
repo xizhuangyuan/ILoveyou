d9a0725cb64dd3e659e5f155c79f905ac9aa911f
xixiaolu <2946679089@qq.com>
2021-11-16 19:48:28 +0800

    修改了部分文件

--- src/home/home.js
+++ src/home/home.js
@@ -2,8 +2,8 @@ import Ani from '../../pub/js/ani';
 import Translate from '../../pub/js/TransLste';
 import Transparency from '../../pub/js/transparency';
 import rount from '../article-rount';
-let animate1 = Ani.create().use(Transparency).mount(document.getElementsByClassName('tit')[0]);
-let animate2 = Ani.create().use(Translate).mount(document.getElementsByClassName('header-center-right_div1_text')[0]);
+Ani.create().use(Transparency).mount(document.getElementsByClassName('tit')[0]); // 动画
+Ani.create().use(Translate).mount(document.getElementsByClassName('header-center-right_div1_text')[0]); // 动画
 var size = 160;        // 多少个字符换行
 var odiv = document.querySelector('.main-center-left');     // 父节点
 var idiv = odiv.querySelector('.template');    //  克隆子节点
@@ -27,14 +27,12 @@ function addtext(text) {
     }
 
     newdiv.querySelector('.btn').querySelector('.details').onclick = function (e) {
-        var a = e.target.parentNode.parentNode.querySelector('h2').dataset.id;
         e.target.parentNode.parentNode.querySelector('.text').innerHTML = text.article.replace(/\n/g, '<br>');
         e.target.parentNode.querySelector('.away').style.display = 'inline-block'
         e.target.style.display = 'none';
     }
 
     newdiv.querySelector('.btn').querySelector('.away').onclick = function (e) {
-        var a = e.target.parentNode.parentNode.querySelector('h2').dataset.id;
         e.target.parentNode.parentNode.querySelector('.text').innerHTML = text.article.ellipsis(size);
         e.target.parentNode.querySelector('.details').style.display = 'inline-block'
         e.target.style.display = 'none'
