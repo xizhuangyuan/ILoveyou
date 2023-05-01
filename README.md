f43ba51c0a820c73fcc9178455ddacb2567ac6ad
xixiaolu <xixiaolu@qq.com>
2023-05-01 10:11:00 +0800

    。。。

--- README.md
+++
@@ -1,5 +0,0 @@
-# xixiaolu.github.io
-这是我的博客
-
-
-<h1>Hello word</h1>
--- index.js
+++
@@ -1,9 +0,0 @@
-import webpackConfig from '../../config/webpack.config.js'
-
-let webpack = new Webpack(webpackConfig);
-
-//作业： 页面输出：
-// 将./src/home/index.js中的代码打包到/docs/home/文件夹中，文件名称叫index.js
-
-
-
